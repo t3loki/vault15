96d59e5d65c2d88c55126db1d6105e544e3d41c5
Danil J <peregar2006@gmail.com>
2014-05-08 23:15:03 +0200

    Dice game update
    Для первого игрока все работает, результаты выводятся в таблицу. Всего
    три раунда для каждого игрока.

--- games/dice/script.js
+++ games/dice/script.js
@@ -1,14 +1,43 @@
-
-
-
-function play1go(btnElem){
+// Player 1 plays
+var count = 0; // counter for tries, max 3
+var sum = 0; // sum of all points for three rounds
+function play1go(btnElem){ 
+	// must make sure that the dice is thrown only 3 times
+	count++;  // counting tries
+	if (count <= 3){
+	  // generating random number between 1 and 12,
+	  // then show this number in the p with id=pl1res
 	var r = Math.floor((Math.random() * 12) + 1);
-	document.getElementById("pl1res").innerHTML = r;
-	// btnElem.innerHTML = "Player1 gets " + r;
+	sum = sum + r;
+	document.getElementById("pl1res").innerHTML = r + " " +count;
+	
+	// every throw has the corresponding entry in the table results
+	switch(count)
+	{
+		case 1:
+			document.getElementById("pl1rnd1score").innerHTML = r
+			break;
+		case 2:
+			document.getElementById("pl1rnd2score").innerHTML = r
+			break;
+		case 3: 
+			document.getElementById("pl1rnd3score").innerHTML = r
+			// when the last 3rd round is played,
+			// output Total score for player 1
+	        document.getElementById("pl1TotalScore").innerHTML = sum;
+			break;  
+	}
+
+	} else {
+		document.getElementById("pl1resInfo").innerHTML = 
+		"No more tries left!"
+	};
 }
 
+// Player 2 plays
 function play2go(btnElem){
 	var r = Math.floor((Math.random() * 12) + 1);
 	document.getElementById("pl2res").innerHTML = r;
-	// btnElem.innerHTML = "Player1 gets " + r;
+	
 }
+
